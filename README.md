0dba9192c6cf040a9d1598aad106dccbc5caf603
BrokenShell <webmaster@sharpdesigndigital.com>
2021-02-14 17:46:56 -0800

    adds ability_dice, ability_check and attack_roll

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# Rust Dice
+# Rusty Dice
 
-## Random Numbers in Rust
+### Random Numbers in Rust
 
-### Features
-### `d(sides: i8) -> i8` 
+## Features
+
+## `d(sides: i8) -> i8` 
 Returns random integer in range [1, sides]
 - Output range: 1-127
 - Max number of sides: 127
@@ -19,7 +20,7 @@ d20:  d(20)  -> [1..20]
 d100: d(100) -> [1..100]
 ```
 
-### `dice(rolls: i8, sides: i8) -> i16` 
+## `dice(rolls: i8, sides: i8) -> i16` 
 Returns random integer in range [rolls, rolls * sides]
 - Output range: 1-16129 (127^2)
 - Max number of sides: 127
@@ -30,3 +31,18 @@ Returns random integer in range [rolls, rolls * sides]
 3d6:  dice(3, 6)  -> [3..18]
 6d8:  dice(6, 8)  -> [6..48]
 ```
+
+## `ability_dice(power: i8) -> i8`
+Returns sum of top 3 rolls of Nd6 where N = power. Used to determine the starting 
+ability scores of characters; strength, dexterity, charisma etc. Always in range 3-18, higher power will produce higher 
+average results. Typically power = 4, but other values (3-9) can be used at the discretion of the DM.
+
+## `ability_check(dc: i8, bonus: i8) -> bool`
+True indicates a success, false indicates a failure. DC ranges from 8 (very easy) to
+25 (nearly impossible). Bonuses range from -5 to +5, typically characters start with zero bonus and retire
+at max level with +5. A negative bonus can be the result of a curse or other negative effect.
+
+## `attack_roll(ac: i8, bonus: i8) -> bool`
+True indicates hit, false indicates miss. AC ranges from 8 (very easy to hit) to
+25 (nearly impossible). Bonuses range from -5 to +5, typically characters start with zero bonus and retire
+at max level with +5. A negative bonus can be the result of a curse or other negative effect.
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,33 @@ mod dice;
 
 
 fn main() {
-    // Single Die
-    println!("\nResult d1: {}, always 1", dice::d(1));
-    println!("Result d10: {}, 1-10 flat uniform", dice::d(10));
-    println!("Result d100: {}, 1-100 flat uniform", dice::d(100));
+    use dice::d;
+    println!("\nSingle Die: d(i8) -> i8");
+    println!("d1: d(1) => {}, always 1", d(1));
+    println!("d10: d(10) => {}, 1-10 flat uniform", d(10));
+    println!("d100: d(100) => {}, 1-100 flat uniform", d(100));
 
-    // Sum multiple rolls of the same size dice
-    println!("\nResult 1d1: {}, always 1", dice::dice(1, 1));
-    println!("Result 10d10: {}, 10-100 geometric", dice::dice(10, 10));
-    println!("Result 100d100: {}, 100-10000 geometric", dice::dice(100, 100));
+    use dice::dice;
+    println!("\nMultiple Dice: dice(i8, i8) -> i16");
+    println!("1d1: dice(1, 1) => {}, always 1", dice(1, 1));
+    println!("10d10: dice(10, 10) => {}, 10-100 geometric", dice(10, 10));
+    println!("100d100: dice(100, 100) => {}, 100-10000 geometric", dice(100, 100));
+
+    use dice::ability_dice;
+    println!("\nAbility Dice: ability_dice(i8) -> i8");
+    println!("Top 3 of 4d6: ability_dice(4) => {}, always 3-18", ability_dice(4));
+    println!("Top 3 of 5d6: ability_dice(5) => {}, always 3-18", ability_dice(5));
+    println!("Top 3 of 6d6: ability_dice(6) => {}, always 3-18", ability_dice(6));
+
+    use dice::ability_check;
+    println!("\nAbility Check: ability_check(i8, i8) -> bool");
+    println!("ability_check(dc=10, bonus=0) => {}", ability_check(10, 0));
+    println!("ability_check(dc=13, bonus=3) => {}", ability_check(13, 3));
+    println!("ability_check(dc=15, bonus=5) => {}", ability_check(15, 5));
+
+    use dice::attack_roll;
+    println!("\nAttack Roll: attack_roll(i8, i8) -> bool");
+    println!("attack_roll(ac=10, bonus=0) => {}", attack_roll(10, 0));
+    println!("attack_roll(ac=13, bonus=3) => {}", attack_roll(13, 3));
+    println!("attack_roll(ac=15, bonus=5) => {}", attack_roll(15, 5));
 }
